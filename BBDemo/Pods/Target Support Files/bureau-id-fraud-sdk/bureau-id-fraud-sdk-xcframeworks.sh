4af31593edeff54ebe31aa59567e898752bf9f1f
Hariharaprabu.M <>
2024-03-26 12:55:04 +0530

    dynamic pod cofig

--- BBDemo/Pods/Target Support Files/bureau-id-fraud-sdk/bureau-id-fraud-sdk-xcframeworks.sh
+++ BBDemo/Pods/Target Support Files/bureau-id-fraud-sdk/bureau-id-fraud-sdk-xcframeworks.sh
@@ -117,5 +117,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../../../bureau-id-fraud-sdk/Framework/bureau_id_fraud_sdk.xcframework" "bureau-id-fraud-sdk" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/bureau-id-fraud-sdk/Framework/bureau_id_fraud_sdk.xcframework" "bureau-id-fraud-sdk" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
