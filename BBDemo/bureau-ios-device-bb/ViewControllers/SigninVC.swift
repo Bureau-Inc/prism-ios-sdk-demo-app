d837f913dd1d3e1a9f2bac0322942944d843238b
Suryakant Sharma <suryakant@bureau.id>
2024-07-16 14:27:22 +0530

    Demo app: new UI changes

--- BBDemo/bureau-ios-device-bb/ViewControllers/SigninVC.swift
+++ BBDemo/bureau-ios-device-bb/ViewControllers/SigninVC.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import bureau_id_fraud_sdk
+import Charts
+import CoreMotion
 
 class SigninVC: BaseViewController {
 
@@ -15,19 +17,38 @@ class SigninVC: BaseViewController {
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var userIDInnerView: UIView!
     @IBOutlet weak var pwdInnerView: UIView!
-
+    @IBOutlet weak var deviceOriendationView: UIView!
     var isBBEnable = false
     var sessionID:String?
+    
+    let motionManager = CMMotionManager()
+    
+    var deviceOriendationGraph:BureauLineGraphView?
+    var counter = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         sessionID = "Demo-"+NSUUID().uuidString
-        BureauAPI.shared.configure(clientID: "***ClientID***", environment: .production, sessionID: sessionID ?? "", enableBehavioralBiometrics: false)
+        BureauAPI.shared.configure(clientID: "***ClientID***", environment: .production, sessionID: sessionID ?? "", enableBehavioralBiometrics: true)
         if isBBEnable{
             BureauAPI.shared.startSubSession(NSUUID().uuidString)
         }
         userIDInnerView.layer.borderColor = UIColor.systemGray5.cgColor
         pwdInnerView.layer.borderColor = UIColor.systemGray5.cgColor
+        initGraph()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        motionManager.stopDeviceMotionUpdates()
+    }
+    
+    func initGraph(){
+        deviceOriendationGraph = BureauLineGraphView(frame: CGRect(x: 0, y: 0, width: deviceOriendationView.frame.width, height: deviceOriendationView.frame.height))
+        deviceOriendationGraph?.titleLbl.text = "User Behavioral Actions"
+        deviceOriendationGraph?.titleIco.isHidden = false
+        self.deviceOriendationView.addSubview(deviceOriendationGraph!)
+        isDeviceMotionAvailable()
     }
     
     @IBAction func signinAct(_ sender: Any) {
@@ -49,6 +70,37 @@ class SigninVC: BaseViewController {
     @IBAction func popAct(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    func isDeviceMotionAvailable(){
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.1  // Update interval in seconds
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { [weak self] (motion, error) in
+                guard let self = self, let motion = motion else { return }
+                
+                // Extract orientation data
+                let (xDegrees, yDegrees, angleDegrees) = self.deviceOrientation(from: motion)
+                deviceOriendationGraph?.Line_1_Lbl.text = "Direction: " + String(format: "%.3f", angleDegrees
+                )+"°"
+                deviceOriendationGraph?.Line_2_Lbl.text = "x: " + String(format: "%.3f", xDegrees)+"°"
+                deviceOriendationGraph?.Line_3_Lbl.text = "y: " + String(format: "%.3f", yDegrees)+"°"
+                deviceOriendationGraph?.Line_4_Lbl.text = " "
+
+                deviceOriendationGraph?.addEntry(randomDouble1: xDegrees, randomDouble2: yDegrees, randomDouble3: angleDegrees)
+            }
+        } else {
+            deviceOriendationGraph?.addEntry(randomDouble1: 0.0, randomDouble2: 0.0, randomDouble3: 0.0)
+            print("Device motion is not available")
+        }
+    }
+    
+    func deviceOrientation(from motion: CMDeviceMotion) -> (Double, Double, Double) {
+        let attitude = motion.attitude
+        let x = attitude.pitch * 180.0 / Double.pi  // Pitch
+        let y = attitude.roll * 180.0 / Double.pi   // Roll
+        let angle = attitude.yaw * 180.0 / Double.pi // Yaw
+        return (x, y, angle)
+    }
+
 }
 
 extension UIViewController{
